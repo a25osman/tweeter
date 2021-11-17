f80920c745424ddd5d77dbf23289bfa690eda6da
Awab Osman <awabosman3@gmail.com>
2021-11-17 18:29:32 -0500

    added tweet frame

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("#tweet-text").on('keyup', function(event) {
+    let count = $(this).val();
+    let remaining = 140 - count.length;
+    
+    const $characterCounter = $(this).siblings("div").find(".counter");
+    if (remaining <= 0){
+      $characterCounter.addClass('red');
+    } else {
+      $characterCounter.removeClass('red');
+    }
+    $characterCounter.html(remaining);
+
+    //
+
+  });
+});
