8988b8159bbe974a340a6ab63ad82940daaea266
Awab Osman <awabosman3@gmail.com>
2021-11-19 15:11:09 -0500

    bug fixes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     let remaining = 140 - count.length;
     
     const $characterCounter = $(this).siblings("div").find(".counter");
-    if (remaining <= 0){
+    if (remaining < 0){
       $characterCounter.addClass('red');
     } else {
       $characterCounter.removeClass('red');
